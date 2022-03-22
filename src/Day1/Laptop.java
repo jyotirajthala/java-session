c0592ef268bb79e06959063c9b031b3b0103a6b8
jyoti <rajthalajyoti9@gmail.com>
2022-03-22 10:08:48 -0400

    laptop object created

---
+++ src/Day1/Laptop.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class Laptop {
+    //States(Variables/ Constant)
+    //model
+    //generation
+    //color
+    //memory
+
+    //Behaviour (Functions / Methods )
+    //start
+    //processing
+    //restart
+    //shutdown
+}
