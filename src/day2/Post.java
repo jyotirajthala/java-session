b00af39f2d0b7117286c69afe86d256a10f4e78b
jyoti <rajthalajyoti9@gmail.com>
2022-03-22 12:14:44 -0400

    day2 package/class created

---
+++ src/day2/Post.java
@@ -0,0 +1,27 @@
+package day2;
+
+public class Post {
+    //state
+
+    //image
+    //video
+    //chooseFilter
+    //edit
+    //captions
+    //tagPeople
+    //locations
+    //numberOfLike
+    //numberOfComment
+    //numberOfShare
+    //postTime
+
+
+    //behaviour
+
+     //like
+    //comment
+    //share
+    //save
+
+
+}
