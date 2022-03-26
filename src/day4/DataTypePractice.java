30a27489e24301b15fad78da5b68f1c6d22603ba
jyoti <rajthalajyoti9@gmail.com>
2022-03-26 11:47:02 -0400

    datatypepractice class created

---
+++ src/day4/DataTypePractice.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class DataTypePractice {
+    static String a;
+    static int b;
+    static boolean c;
+    public static void main(String[] args){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
