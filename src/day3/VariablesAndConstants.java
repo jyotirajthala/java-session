68378c69adb34e07d18490a2ec1a3ed860a1654e
jyoti <rajthalajyoti9@gmail.com>
2022-03-26 11:08:19 -0400

    comment added

--- src/day3/VariablesAndConstants.java
+++ src/day3/VariablesAndConstants.java
@@ -3,28 +3,36 @@ package day3;
 public class VariablesAndConstants {
 
     public static void main(String[] args){
-        String postId = "insta1"; // Variable declare
-        postId = "insta2";
+        String postId = "instagram1"; // Variable declare
+        postId = "instagram2";
 
         final String TITLE = "This is constant";
         //TITLE = "Constant value can't be changed";
 
+        //constant
         final String SMALL= "S";
         final String MEDIUM= "M";
         final String LARGE= "L";
         final String EXTRA_LARGE= "XL";
 
+        //enum replaces multiple constant
         enum SIZE {
             SMALL, MEDIUM, LARGE, EXTRA_LARGE
         }
 
+        //assignment operator
         int x= 45;
         x+=9; // x= x+9;
+        System.out.println(x);
         x-=9; //x= x-9;
+        System.out.println(x);
         x*=9; //x= x*9;
+        System.out.println(x);
         x/=9; //x= x/9;
+        System.out.println(x);
 
 
+        //arithmetic operation
         int a= 7;
         int b= 9;
         int c= 5;
