e90c8e2220581e43f7f45e43d3f0d7d2c568800b
jyoti <rajthalajyoti9@gmail.com>
2022-04-20 20:20:01 -0400

    blockscoop conditional statement loop switch statement created

---
+++ src/day13/Loop.java
@@ -0,0 +1,22 @@
+package day13;
+
+public class Loop {
+    public static void main(String[] args) {
+        int index = 1001;
+
+//        while (index <= 1000){
+//            System.out.println(index);
+//            index += 100;
+//        }
+
+//        do {
+//            System.out.println(index);
+//            index += 100;
+//        }while (index <= 1000);
+
+        for (int i = 1; i <= 1000 ; i++){
+            System.out.println(i);
+        }
+
+    }
+}
