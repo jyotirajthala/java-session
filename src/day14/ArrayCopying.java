f87a1fdca30c1d2a61f6c9e2682224be253175a7
jyoti <rajthalajyoti9@gmail.com>
2022-04-20 20:26:42 -0400

    array coping big number practice

---
+++ src/day14/ArrayCopying.java
@@ -0,0 +1,19 @@
+package day14;
+
+import java.util.Arrays;
+
+public class ArrayCopying {
+    public static void main(String[] args) {
+        int[] smallPrimes = { 2, 3, 5, 7, 11, 13 };
+
+//        int[] smallPrimesCopy = smallPrimes;
+        int[] smallPrimesCopy = Arrays.copyOf(smallPrimes, 10);
+
+//        smallPrimesCopy[0] = 0;
+
+        for (int smallPrime : smallPrimesCopy) {
+            System.out.println(smallPrime);
+        }
+
+    }
+}
